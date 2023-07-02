2ce9747ef9fc10428ac091bed951c2305e9a264d
Mikaruz <gersonsebas2019@gmail.com>
2023-07-02 13:39:23 -0500

    port3306

--- peraroja/admin/config/db.php
+++ peraroja/admin/config/db.php
@@ -5,9 +5,9 @@ $user = "root";
 $password = "";
 
 try {
-    $conection = new PDO("mysql:host=$server;port=3307;dbname=$db", $user, $password);
+    $conection = new PDO("mysql:host=$server;port=3306;dbname=$db", $user, $password);
 } catch (Exception $ex) {
     echo $ex->getMessage();
 }
 
-$conexion = mysqli_connect("localhost", "root", "", "peraroja") or die("error de conexion");
+$conexion = mysqli_connect("localhost:3306", "root", "", "peraroja") or die("error de conexion");
